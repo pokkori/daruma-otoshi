d5e7a2bf0afe45f18340848f2e9f31e650b7a49f
pokkori <reborna2017@yahoo.co.jp>
2026-03-19 01:16:46 +0900

    fix: 特商法ページ - 決済会社名を汎用表記に更新（審査対応）

---
+++ twa/app/src/main/java/jp/pokkori/daruma/DelegationService.java
@@ -0,0 +1,14 @@
+package jp.pokkori.daruma;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
